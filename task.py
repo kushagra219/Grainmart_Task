1d5ceb1f74f0ca5e0e26e09793c7773c3324f6b8
Kushagra Garg <kushagra.garg1291@gmail.com>
2020-05-17 17:19:10 +0530

    added selenium approach

--- task.py
+++ task.py
@@ -1,20 +1,39 @@
 from requests_html import HTMLSession
 from scrapy.selector import Selector
+from selenium import webdriver
 
 # initialize an HTTP session
-session = HTMLSession()
+driver = webdriver.Firefox()
 
 # url to be scraped
 url = "https://www.dramexchange.com/"
 
 # GET Request
-res = session.get(url)
+driver.get(url)
 
-# Intializing a scrapy selector object
-sel = Selector(text=res.text)
+# # Extracting value from Xpath
+value = driver.find_element_by_xpath("//*[@id='tb_NationalDramSpotPrice']/tr[3]/td[5]")
 
-# Extracting value from Xpath
-value = sel.xpath("//*[@id='tb_NationalDramSpotPrice']/tr[3]/td[5]/text()").get()
+# # Printing the required value
+print(value.text)
 
-# Printing the required value
-print(value)
+
+## SECOND APPROACH USING SCRAPY ##
+
+# # initialize an HTTP session
+# session = HTMLSession()
+
+# # url to be scraped
+# url = "https://www.dramexchange.com/"
+
+# # GET Request
+# res = session.get(url)
+
+# # Intializing a scrapy selector object
+# sel = Selector(text=res.text)
+
+# # Extracting value from Xpath
+# value = sel.xpath("//*[@id='tb_NationalDramSpotPrice']/tr[3]/td[5]/text()").get()
+
+# # Printing the required value
+# print(value)
